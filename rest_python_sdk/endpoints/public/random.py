dcfb886791dc1327bc42b5030a151e28a03b184c
Labret <jfr.labret@hotmail.com>
2023-02-17 23:05:31 +0100

    Fix imports

---
+++ rest_python_sdk/endpoints/public/random.py
@@ -0,0 +1,11 @@
+import typing as t
+
+from rest_python_sdk.restclient import RESTClient
+
+_endpoint: str = "public/random/"
+
+def string(payload: dict[str, t.Any], rest: RESTClient):
+    return rest.post(f"{_endpoint}string", payload=payload)
+
+def number(payload: dict[str, t.Any], rest: RESTClient):
+    return rest.post(f"{_endpoint}number", payload=payload)
---
+++ rest_python_sdk/endpoints/public/utils.py
@@ -0,0 +1,14 @@
+import typing as t
+
+from rest_python_sdk.restclient import RESTClient
+
+_endpoint: str = "public/utils/"
+
+def encode(payload: dict[str, t.Any], rest: RESTClient):
+    return rest.post(f"{_endpoint}encode", payload=payload)
+
+def decode(payload: dict[str, t.Any], rest: RESTClient):
+    return rest.post(f"{_endpoint}decode", payload=payload)
+
+def hash(payload: dict[str, t.Any], rest: RESTClient):
+    return rest.post(f"{_endpoint}hash", payload=payload)
