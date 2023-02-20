4cc8c26c3334bdff3d141bc8eef13658013c0236
Labret <jfr.labret@hotmail.com>
2023-02-20 14:31:57 +0100

    Change Response to APIResponse, add ResponseHeaders to APIResponse

--- rest_python_sdk/models/response.py
+++ rest_python_sdk/models/response.py
@@ -1,29 +1,32 @@
 from __future__ import annotations
 
 __all__ = [
-    "Response",
+    "APIResponse",
 ]
 
 import typing as t
 from dataclasses import dataclass
 
 from rest_python_sdk.errors.errors import send_error_response
+from rest_python_sdk.models.response_headers import ResponseHeaders
 
 
 @dataclass
-class Response:
+class APIResponse:
     _status: str
     _code: int
     _message: str
     _data: t.Any
+    _headers: ResponseHeaders
 
-    def __init__(self, data: dict[str, t.Any]) -> None:
+    def __init__(self, data: dict[str, t.Any], headers: dict[str, str]) -> None:
         if data["code"] >= 400:
             raise send_error_response(data)
         self.status = data["status"]
         self.code = data["code"]
         self.message = data["message"]
         self.data = data["data"]
+        self.headers = ResponseHeaders(headers)
 
     def __str__(self) -> str:
         return f"Status: {self.status}\nCode: {self.code}\nMessage: {self.message}\nData: {self.data}"
@@ -59,3 +62,11 @@ class Response:
     @data.setter
     def data(self, value):
         self._data = value
+    
+    @property
+    def headers(self):
+        return self._headers
+
+    @headers.setter
+    def headers(self, value):
+        self._headers = value
