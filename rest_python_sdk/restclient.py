f59a46a130e95b981d30ede9d28792fa3f9b1199
Labret <jfr.labret@hotmail.com>
2023-04-07 23:36:00 +0200

    Add xml kwarg to base requests

--- rest_python_sdk/restclient.py
+++ rest_python_sdk/restclient.py
@@ -95,19 +95,32 @@ class RESTClient:
         endpoint: str,
         payload: t.Optional[dict[str, t.Any]] = None,
         return_headers: bool = False,
+        xml: bool = False,
     ) -> APIResponse:
+        xml_string = ""
         r = requests.request(
             method, f"{self.base_url}{endpoint}", headers=self.headers, json=payload
         )
         if r.status_code == 404:
             resp = {"code": r.status_code, "note": f"{r.url} {r.reason}"}
             raise send_error_response(resp)
+        if xml:
+            if "?" in endpoint:
+                xml_query_string = "&xml"
+            else:
+                xml_query_string = "?xml"
+            xml_string = requests.request(
+                method,
+                f"{self.base_url + endpoint + xml_query_string}",
+                headers=self.headers,
+                json=payload,
+            ).text
         if not return_headers:
-            return APIResponse(r.json())
+            return APIResponse(r.json(), xml=xml_string)
         else:
-            return APIResponse(r.json(), r.headers)
+            return APIResponse(r.json(), r.headers, xml=xml_string)
 
-    def get(self, endpoint: str, return_headers: bool = False):
+    def get(self, endpoint: str, return_headers: bool = False, xml: bool = False):
         """
         Synchronous GET request.
 
@@ -118,13 +131,14 @@ class RESTClient:
         Returns:
             `APIResponse`: The object created from the response.
         """
-        return self._request("GET", endpoint, return_headers=return_headers)
+        return self._request("GET", endpoint, return_headers=return_headers, xml=xml)
 
     def post(
         self,
         endpoint: str,
         payload: dict[str, t.Any],
         return_headers: bool = False,
+        xml: bool = False,
     ):
         """
         Synchronous POST request.
@@ -137,13 +151,16 @@ class RESTClient:
         Returns:
             `APIResponse`: The object created from the response.
         """
-        return self._request("POST", endpoint, payload, return_headers=return_headers)
+        return self._request(
+            "POST", endpoint, payload, return_headers=return_headers, xml=xml
+        )
 
     def put(
         self,
         endpoint: str,
         payload: dict[str, t.Any],
         return_headers: bool = False,
+        xml: bool = False,
     ):
         """
         Synchronous PUT request.
@@ -156,9 +173,11 @@ class RESTClient:
         Returns:
             `APIResponse`: The object created from the response.
         """
-        return self._request("PUT", endpoint, payload, return_headers=return_headers)
+        return self._request(
+            "PUT", endpoint, payload, return_headers=return_headers, xml=xml
+        )
 
-    def delete(self, endpoint: str, return_headers: bool = False):
+    def delete(self, endpoint: str, return_headers: bool = False, xml: bool = False):
         """
         Synchronous DELETE request.
 
@@ -169,7 +188,7 @@ class RESTClient:
         Returns:
             `APIResponse`: The object created from the response.
         """
-        return self._request("DELETE", endpoint, return_headers=return_headers)
+        return self._request("DELETE", endpoint, return_headers=return_headers, xml=xml)
 
     async def _async_request(
         self,
@@ -185,9 +204,9 @@ class RESTClient:
                 resp = {"code": r.status, "note": f"{r.url} {r.reason}"}
                 raise send_error_response(resp)
             if not return_headers:
-                return APIResponse(await r.json())
+                return APIResponse(await r.json(), xml="")
             else:
-                return APIResponse(await r.json(), r.headers)
+                return APIResponse(await r.json(), r.headers, xml="")
 
     async def async_get(self, endpoint: str, *, return_headers: bool = False):
         """
